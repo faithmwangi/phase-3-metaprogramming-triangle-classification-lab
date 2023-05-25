1d7ae61b7d87cb528fa5c5f01a50b85a863090bb
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-05-25 04:19:20 +0300

    Don

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    validate_triangle
+
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      raise TriangleError, "Side lengths must be greater than 0"
+    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+      raise TriangleError, "Invalid triangle: sum of any two sides must be greater than the third side"
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangles"
+    end
+  end
 end
